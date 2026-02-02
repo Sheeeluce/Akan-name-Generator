8818fe67d82c420a7ae7ab23f918d501fc54494e
Lucy <lucymwago9@gmail.com>
2026-02-02 22:23:00 +0300

    add calculation

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const monthSelect = document.getElementById("MM")
 const genderSelect = document.getElementById("gender")
 const resultBox = document.getElementById("result-box")
 const akanName = document.getElementById("akan-name")
+
+function calculateAkan() {
+  const year = parseInt(yearInput.value);
+  const day = parseInt(dateInput.value);
+  const month = parseInt(monthSelect.value);
+  const gender = (genderSelect.value);
+
+  const CC = Math.floor(year / 100 );
+  const YY = year % 100;
+
+  const d = ((4*CC - 2*(CC - 1)) + (45*YY) + (1026*(month + 1)) + day) % 7;
+}
