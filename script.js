7bb5752a8fa7f46e97d6a3f58ead8a9a7391ebb1
Lucy <lucymwago9@gmail.com>
2026-02-02 21:46:38 +0300

    add result box

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-  
+  //DOM ELEMENTS
+const yearInput = document.getElementById("YY")
+const dateInput = document.getElementById("DD")
+const monthSelect = document.getElementById("MM")
+const    = document.getElementById("YY")
+const yearInput = document.getElementById("YY")
