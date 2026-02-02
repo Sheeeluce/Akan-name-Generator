40132a470fe9b050b41d0ec09b4ed5ef70c9d5c1
Lucy <lucymwago9@gmail.com>
2026-02-02 22:37:16 +0300

    add alert messages

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const genderSelect = document.getElementById("gender")
 const resultBox = document.getElementById("result-box")
 const akanName = document.getElementById("akan-name")
 
-function calculateAkan() {
+function generateAkanName() {
   const year = parseInt(yearInput.value);
   const day = parseInt(dateInput.value);
   const month = parseInt(monthSelect.value);
@@ -16,4 +16,26 @@ function calculateAkan() {
   const YY = year % 100;
 
   const d = ((4*CC - 2*(CC - 1)) + (45*YY) + (1026*(month + 1)) + day) % 7;
+
+  const male = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  const female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+  const name = gender === "male" ? male[d] : female[d];
+
+  akanName.textContent = akanname;
+
+  if (!YY) {
+    alert("Please enter year of birth");
+    return;
+  }
+
+  if (!DD) {
+    alert("Please enter date of birth");
+    return;
+  }
+
+  if (!MM) {
+    alert("Please enter month of birth");
+    return;
+  }
 }
